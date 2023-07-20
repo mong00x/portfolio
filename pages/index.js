708cebfcae1eac720acd132bdbe12df7c46202ab
Meng Yuan <yuanmeng1206@gmail.com>
2023-07-20 15:15:55 +0930

    move qr button to footer

--- pages/index.js
+++ pages/index.js
@@ -65,20 +65,7 @@ export default function Home() {
             </button> */}
             {/* <div className={styles.navBtnGroup}></div> */}
             <div className={styles.navBtnContainer}>
-            <Popover placement="auto">
-              <PopoverTrigger>
-              <button className={styles.navBtn}>
-                <ion-icon name="qr-code-outline"></ion-icon>
-              </button>
-              </PopoverTrigger>
-              <PopoverContent  mr="4px" p="12px" gap="12px" bg={theme === "light" ? "white" : "black"} borderRadius={12} color={theme === "light" ? "#0f2b33" : "#f8f8f8"} >
-                <Flex flexDir="row-reverse" gap="24px" w="100%" justifyContent="space-between">
-                  <PopoverCloseButton borderRadius="12px"  fontSize="1rem" w="2.5rem" h="2.5rem" alignSelf="flex-end"/>
-                  <PopoverHeader fontSize="20px">Check it on your phone</PopoverHeader>
-                </Flex>
-                <Image src="/qrcode.png" alt="qrcode" w="70vw" maxW="360px" m="auto"/>
-              </PopoverContent>
-            </Popover>
+           
              
               <button className={styles.navBtn} onClick={switchTheme}>
                 {theme === "light" ? (
@@ -152,6 +139,7 @@ export default function Home() {
         </main>
         
         <footer className={styles.footer}>
+          
         <svg 
         opacity={theme === "light" ? "0.8" : "0.3"}
         
@@ -194,6 +182,20 @@ export default function Home() {
                   <a className={styles.devpost} href="https://devpost.com/mong00x?ref_content=user-portfolio&ref_feature=portfolio&ref_medium=global-nav">
                     Devpost
                   </a>
+                  <Popover placement="auto">
+                    <PopoverTrigger>
+                    <button className={styles.qrBtn}>
+                      <ion-icon name="qr-code-outline"></ion-icon>
+                    </button>
+                    </PopoverTrigger>
+                    <PopoverContent  mr="4px" p="12px" gap="12px" bg={theme === "light" ? "white" : "black"} borderRadius={12} color={theme === "light" ? "#0f2b33" : "#f8f8f8"} >
+                      <Flex flexDir="row-reverse" gap="24px" w="100%" justifyContent="space-between">
+                        <PopoverCloseButton borderRadius="12px"  fontSize="1rem" w="2.5rem" h="2.5rem" alignSelf="flex-end"/>
+                        <PopoverHeader fontSize="20px">Check it on your phone</PopoverHeader>
+                      </Flex>
+                      <Image src="/qrcode.png" alt="qrcode" w="70vw" maxW="360px" m="auto"/>
+                    </PopoverContent>
+                  </Popover>
                 </div>
               </div>
             </div>
